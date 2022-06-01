abef42eabda70a3fda45b714007c48fb68d2bf34
Eliott Rajaud <eliott.rajaud@cpe.fr>
2022-06-01 11:27:54 +0200

    point facility

--- front/js/facility.js
+++ front/js/facility.js
@@ -10,15 +10,39 @@ function GetFacilities(){
                     };
     fetch(GET_CHUCK_URL,context)
         .then(response => response.json())
-            .then(response => DisplayFacilities(response))
+            .then(response => DisplayFacilities(response)) //printFacilities(facility)
             .catch(error => err_callback(error));
 }
 
 function DisplayFacilities(facility){
-    console.log(facility)
+    console.log(facility[0])
+    printFacilities(facility)
     console.log("OUIUIUIUIU");
 }
 
 function err_callback(error){
     console.log(error);
+}
+var marker;
+var myIcon = L.icon({
+    iconUrl: '../../img/cas1.png',
+    iconSize: [35, 35],
+    iconAnchor: [22, 94],
+    popupAnchor: [-3, -76],
+
+    });
+
+function printFacilities(facility){
+    for (let i = 0; i < facility.length; i++) {
+        if((facility[i].name).includes("Cas5")){
+            marker = L.marker([facility[i].lat, facility[i].lon], {
+            icon: myIcon
+            }).addTo(map);
+            
+        }
+        else{
+            marker = L.marker([facility[i].lat, facility[i].lon]).addTo(map);
+        }
+      }
+     
 }
