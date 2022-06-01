0db7cc2c6d142ec37ca5dedc1006b762c694d978
Elliot Garcia <elliotgarciabrewer@gmail.com>
2022-06-01 09:22:13 +0200

    Map

---
+++ front/js/map.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([45.764043, 4.835659], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
