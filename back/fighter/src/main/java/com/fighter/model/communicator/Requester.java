7eb0a93d43508d565efb9e9a1be248f0b8b68fb2
vick <vick.foex@cpe.fr>
2022-06-02 12:38:41 +0200

    YAY

--- back/fighter/src/main/java/com/fighter/model/communicator/Requester.java
+++ back/fighter/src/main/java/com/fighter/model/communicator/Requester.java
@@ -1,5 +1,8 @@
 package com.fighter.model.communicator;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.client.RestTemplate;
 
 import com.fighter.model.dto.FacilityDto;
@@ -48,5 +51,33 @@ public class Requester {
 		FacilityDto result = restTemplate.getForObject(url, FacilityDto.class);
 		return result;
 	}
+	
+	public static void putVehicule( VehiculeDto body) {
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Accept", MediaType.APPLICATION_JSON_VALUE);
+
+		RestTemplate restTemplate = new RestTemplate();
+
+		// Data attached to the request.
+		HttpEntity<VehiculeDto> requestBody = new HttpEntity<>(body, headers);
+		
+		System.out.println("oui");
+
+		// Send request with PUT method.
+		restTemplate.put(ConstantURL.getVehiculeurl(), requestBody, new Object[] {});
+		
+		System.out.println("oui");
+
+
+		String resourceUrl = ConstantURL.getVehiculeurl() + "/" + ConstantURL.teamCode + "/" + body.getId();
+		
+		System.out.println(resourceUrl);
+
+		String e = restTemplate.getForObject(resourceUrl, String.class);
+
+		System.out.println(e);
+	}
 
 }
+
--- back/fighter/src/main/java/com/fighter/model/modelVehicule/Vehicule.java
+++ back/fighter/src/main/java/com/fighter/model/modelVehicule/Vehicule.java
@@ -1,9 +1,15 @@
 package com.fighter.model.modelVehicule;
 
+import com.fighter.model.dto.VehiculeDto;
+
 public class Vehicule extends AbstractVehicule {
 
 	Vehicule() {
 		super();
+	}
+	public Vehicule(VehiculeDto v) {
+		super();
+		this.vehicule = v;
 	}
 
 	@Override
