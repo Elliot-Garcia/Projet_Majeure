30d09326bf89bc6283f0163081583f5681f3a497
vick <vick.foex@cpe.fr>
2022-06-02 12:23:33 +0200

    put vick

--- back/fighter/src/main/java/com/fighter/model/communicator/ConstantURL.java
+++ back/fighter/src/main/java/com/fighter/model/communicator/ConstantURL.java
@@ -3,8 +3,12 @@ package com.fighter.model.communicator;
 public class ConstantURL {
 	
 	private static final String FireURL = "http://vps.cpe-sn.fr:8081/fire/";
-	private static final String VehiculeURL = "http://vps.cpe-sn.fr:8081/vehicle/";;
+	private static final String VehiculeURL = "http://vps.cpe-sn.fr:8081/vehicle/";
 	private static final String FacilityURL = "http://vps.cpe-sn.fr:8081/facility/";
+
+	
+	public static final String teamCode = "b6b21258-84b8-42dd-bdf4-35014914c964";
+	
 	
 	
 	public static String getFireurl() {
---
+++ back/fighter/src/main/java/org/o7planning/sbrestfulclient/put/Requester.java
@@ -0,0 +1,78 @@
+package com.fighter.model.communicator;
+
+import org.springframework.web.client.RestTemplate;
+
+import com.fighter.model.communicator.ConstantURL;
+import com.fighter.model.dto.FacilityDto;
+import com.fighter.model.dto.FireDto;
+import com.fighter.model.dto.VehiculeDto;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.RestTemplate;
+
+
+public class Requester {
+	
+	public static FireDto[] requestFire( ) {
+		
+		RestTemplate restTemplate = new RestTemplate();
+		FireDto[] result = restTemplate.getForObject(ConstantURL.getFireurl(), FireDto[].class);
+		return result;
+	}
+	
+	public static FireDto requestFireByID( int id ) {
+
+		RestTemplate restTemplate = new RestTemplate();
+		String url = ConstantURL.getFireurl() + id;
+		
+		FireDto result = restTemplate.getForObject(url, FireDto.class);
+
+		return result;
+	}
+	
+	
+	public static VehiculeDto[] requestVehicule( ) {
+		
+		RestTemplate restTemplate = new RestTemplate();
+		VehiculeDto[] result = restTemplate.getForObject(ConstantURL.getVehiculeurl(), VehiculeDto[].class);
+		return result;
+	}
+	
+	public static FacilityDto[] requestFacility( ) {
+		
+		RestTemplate restTemplate = new RestTemplate();
+		FacilityDto[] result = restTemplate.getForObject(ConstantURL.getFacilityurl(), FacilityDto[].class);
+		return result;
+	}
+	
+	public static FacilityDto requestFacilityByID( int id ) {
+		
+		RestTemplate restTemplate = new RestTemplate();
+		String url = ConstantURL.getFacilityurl() + id;
+		FacilityDto result = restTemplate.getForObject(url, FacilityDto.class);
+		return result;
+	}
+
+	public static void putVehicule( VehiculeDto body, int id ) {
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Accept", MediaType.APPLICATION_JSON_VALUE);
+
+		RestTemplate restTemplate = new RestTemplate();
+
+		// Data attached to the request.
+		HttpEntity<VehiculeDto> requestBody = new HttpEntity<>(body, headers);
+
+		// Send request with PUT method.
+		restTemplate.put(ConstantURL.getVehiculeurl(), requestBody, new Object[] {});
+
+		String resourceUrl = ConstantURL.getVehiculeurl() + "/" + ConstantURL.teamCode + "/" + body.getId();
+
+		String e = restTemplate.getForObject(resourceUrl, String.class);
+
+		System.out.println(e);
+	}
+
+}
