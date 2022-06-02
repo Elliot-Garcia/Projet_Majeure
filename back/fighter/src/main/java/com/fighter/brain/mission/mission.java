c7a761586b44dee2214f6c9123bccf95b28cb967
Elliot Garcia <elliotgarciabrewer@gmail.com>
2022-06-02 12:21:14 +0200

    f

--- back/fighter/src/main/java/com/fighter/brain/mission/mission.java
+++ back/fighter/src/main/java/com/fighter/brain/mission/mission.java
@@ -1,5 +1,6 @@
 package com.fighter.brain.mission;
 
+import com.fighter.model.communicator.Requester;
 import com.fighter.model.dto.FireDto;
 import com.fighter.model.dto.VehiculeDto;
 import com.fighter.model.modelVehicule.InterfaceVehicule;
@@ -17,11 +18,13 @@ public class mission {
 	public void debutMission() {
 		this.vehicule.getVehiculeDto().setLat(this.feu.getLat());
 		this.vehicule.getVehiculeDto().setLon(this.feu.getLon());
+		Requester.putVehicule(this.vehicule.getVehiculeDto());
 	}
 	
 	public void finMission() {
 		this.vehicule.getVehiculeDto().setLat(this.vehicule.findFacilityById(this.vehicule.getVehiculeDto().getFacilityRefID()).getLat());
 		this.vehicule.getVehiculeDto().setLon(this.vehicule.findFacilityById(this.vehicule.getVehiculeDto().getFacilityRefID()).getLon());
+		Requester.putVehicule(this.vehicule.getVehiculeDto());
 	}
 	
 }
